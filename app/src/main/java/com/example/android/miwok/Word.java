522f25d14675999621fa62cb2c83c210e36814ef
Abdulla <abdulla.shoshaa@gmail.com>
2016-08-13 23:40:02 +0300

    fix adding classes to staging area

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,4 +5,30 @@ package com.example.android.miwok;
  */
 
 public class Word {
+
+    private String mEnglishTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String englishWord, String miwokWord){
+
+        this.mEnglishTranslation = englishWord;
+        this.mMiwokTranslation = miwokWord;
+
+    }
+
+    public String getEnglishTranslation() {
+        return mEnglishTranslation;
+    }
+
+    public void setEnglishTranslation(String mEnglishTranslation) {
+        this.mEnglishTranslation = mEnglishTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public void setMiwokTranslation(String miwokTranslation) {
+        this.mMiwokTranslation = miwokTranslation;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,8 +1,41 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Shoshaa on 8/9/16.
  */
 
-public class WordAdapter {
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Context context, List<Word> objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+
+        Word word = getItem(position);
+
+        TextView englishTextView = (TextView) view.findViewById(R.id.english_word);
+        englishTextView.setText(word.getEnglishTranslation());
+
+        TextView miwokTextView = (TextView) view.findViewById(R.id.miwok_word);
+        miwokTextView.setText(word.getMiwokTranslation());
+
+        return view;
+    }
 }
